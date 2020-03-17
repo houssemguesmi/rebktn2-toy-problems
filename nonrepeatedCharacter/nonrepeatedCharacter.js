ede87459b16de5dc08e0371e57e19825c5f2638d
houssemguesmi <houssemguesmi14@gmail.com>
2020-03-17 14:23:48 +0100

    ToyProbSecondSolution

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -6,6 +6,7 @@
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
 
+///// SOLUTION with O(n*n)
 var firstNonRepeatedCharacter = function(string) {
   let array = string.split('')
   let charactersandoccurences = []
@@ -29,3 +30,37 @@ var firstNonRepeatedCharacter = function(string) {
     }
   }
 };
+
+firstNonRepeatedCharacter('AXAAB') ==> 'X'
+
+///// SOLUTION with O(n)
+function min(array) {
+  let min = array[0];
+  for(let i = 1; i < array.length; i++) {
+    if(array[i] < min) {
+      min = array[i];
+    }
+  }
+  return min;
+}
+
+var firstNonRepeatedCharacter = function(string) {
+  let array = new Array(100)
+  let oneOccurences = []
+  for(let i = 0; i < array.length; i++) {
+    array[i] = {character: undefined, index: 0, no: 0}
+  }
+  for(let i = 0; i < string.length; i++) {
+    array[string[i].charCodeAt()].character = string[i];
+    array[string[i].charCodeAt()].index = i;
+    array[string[i].charCodeAt()].no++;
+  }
+  for(let j = 0; j < array.length; j++) {
+    if(array[j].no === 1){
+      oneOccurences.push(array[j].index)
+    }
+  }
+  return string[min(oneOccurences)];
+}
+
+firstNonRepeatedCharacter('AXAAB') ==> 'X'
